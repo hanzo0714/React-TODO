875f63b1b4286f10f1b16e5746b26c30c056b4ef
hanzo0714 <107672949+hanzo0714@users.noreply.github.com>
2022-06-28 15:35:13 +0900

    TODO５個まで機能

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録は５個までです。消化してください。</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickDelete={onClickDelete}
